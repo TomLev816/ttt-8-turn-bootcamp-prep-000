ce5d4f0612e8e6f5bd67366bdb641da6cccebffb
Thomas Lever <thomaslever508@gmail.com>
2018-07-06 16:26:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,8 +9,8 @@ end
 def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board,index) == false
       return true
-  else 
-    return false
+  else
+      return false
   end
 end
 

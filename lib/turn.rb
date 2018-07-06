58a53772936937b11f911e51dcc5d250d29bdcf9
Thomas Lever <thomaslever508@gmail.com>
2018-07-06 16:24:35 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+def input_to_index(user_input)
+  converted_input = user_input.to_i - 1
+  return converted_input
+end
